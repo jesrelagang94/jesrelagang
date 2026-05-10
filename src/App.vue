3349ae05a9bffa2d5b2a520acf698e23ad4a5a8b
Jesrel Agang <130927892+jesrelagang94@users.noreply.github.com>
2026-05-10 10:05:19 +0800

    feat(multi-page): wire SiteNav + SiteFooter into App.vue (public routes only)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <PreLoader v-if="!isAdminRoute" />
+  <SiteNav v-if="!isAdminRoute" />
   <router-view />
+  <SiteFooter v-if="!isAdminRoute" />
   <Toast />
   <CookieConsent v-if="!isAdminRoute" @consent-given="handleConsent" />
 </template>
@@ -9,6 +11,8 @@
 import { computed, ref } from "vue";
 import { useRoute } from "vue-router";
 import PreLoader from "./components/layout/PreLoader.vue";
+import SiteNav from "./components/layout/SiteNav.vue";
+import SiteFooter from "./components/layout/SiteFooter.vue";
 import Toast from "./components/ui/Toast.vue";
 import CookieConsent from "./components/CookieConsent.vue";
 import { dataImage, portfolioHover, scroll_, stickyNav } from "./utilits";
@@ -23,10 +27,8 @@ export default {
     const isAdminRoute = computed(() => route.path.startsWith('/admin'));
     const consentGiven = ref(false);
 
-    // Handle consent changes
     const handleConsent = (consent) => {
       consentGiven.value = true;
-      // If tracking consent was given, track the visitor
       if (consent.tracking) {
         trackVisitor();
       }
@@ -38,7 +40,6 @@ export default {
     return { dark: false };
   },
   mounted() {
-    // Only run portfolio utilities on non-admin routes
     if (!this.isAdminRoute) {
       window.addEventListener("scroll", stickyNav);
       window.addEventListener("scroll", scroll_);
@@ -48,13 +49,12 @@ export default {
         new WOW().init();
       }, 100);
 
-      // Import accessibility CSS
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.href = '/css/accessibility.css';
       document.head.appendChild(link);
     }
   },
-  components: { PreLoader, Toast, CookieConsent },
+  components: { PreLoader, SiteNav, SiteFooter, Toast, CookieConsent },
 };
 </script>
